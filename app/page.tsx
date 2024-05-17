6b25a1c822f05cabb85d616ce6f29cb9d3770f46
Punyapat Chanthakhun <zunzuza212547@gmail.com>
2024-05-17 21:42:33 +0700

    add many thing

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code"
 import { Button } from "@nextui-org/react";
 import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site"
+import { siteConfig } from "../config/site"
 import { title, subtitle } from "../components/primitives";
 import playbuttonicon from "../public/playbuttonicon.svg"
 import Spline from '@splinetool/react-spline';
